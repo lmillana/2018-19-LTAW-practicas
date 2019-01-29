42f0d80474f935bda5b4ae0d17256a6d5a3235d6
Leyree <leyremc@MacBook-Pro-de-Leyree.local>
2019-01-29 12:30:17 +0100

    Hecho en clase, ¡seguir en casa!

--- Practica-1/hello.js
+++ Practica-1/hello.js
@@ -1,4 +1,4 @@
-# Probando desde MacOs:
+//Probando desde MacOs:
 console.log("Hola como estás");
 
 process.stdout.write("Otra forma de IMPRIMIR \n\n")
---
+++ Practica-1/server.js
@@ -0,0 +1,13 @@
+// SERVER.js
+var http = require('http');
+
+console.log("Arrancando servidor...")
+
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
+  console.log("Peticion atendida")
+}).listen(8080);
+
+// NAVEGADOR LOCAL: http://localhost:8080/
