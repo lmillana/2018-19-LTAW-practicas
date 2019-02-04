604b2b770d60787d76be2ae1a896b280bc7b1dd6
Leyree <leyremc@MacBook-Pro-de-Leyree.local>
2019-02-04 23:37:17 +0100

    ¡Cogiendo color! Poquito a poco.

--- Practica-1/server.js
+++ Practica-1/server.js
@@ -1,5 +1,9 @@
 // SERVER.js
+// This file opens the request file and returns the content to the client.
+// If anything goes wrong, throw a 404 error.
 var http = require('http');
+var url = require('url');
+var fs = require('fs');
 
 console.log("Arrancando servidor...")
 
@@ -7,12 +11,23 @@ console.log("Arrancando servidor...")
 http.createServer(function (req, res) {
   console.log("--> Petición RECIBIDA!");
 
-  console.log("Recurso solicitado(URL): " + req.url);
-  // Imprimir en la misma URL:
-  //res.end(req.url);
-  console.log();
-  console.log("--> Peticion ATENDIDA!");
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname; //filename: ./xxx.// XXX:
+  fs.readFile(filename, function(err, data){
+    console.log("Recurso solicitado(URL): " + filename);
+    console.log();
 
+    if (err){
+      res.end("404 NOT FOUND");
+    } else {
+      res.write(data);
+      res.end();
+      console.log("--> Peticion ATENDIDA!");
+    }
+  });
 }).listen(8080);
 
-// NAVEGADOR LOCAL: http://localhost:8080/
+// NAVEGADOR LOCAL: http://localhost:8080/xxx
+
+// Imprimir en la misma URL:
+//res.end(req.url);
