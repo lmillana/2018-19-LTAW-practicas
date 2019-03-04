851d1796ea9867fe83e2a2cec71f5c40dbcd8c17
Leyree <leyremc@MacBook-Pro-de-Leyree.local>
2019-03-04 20:15:14 +0100

    ¡Funciona!

---
+++ Practica-2/mi_proyectoweb/mi_tienda/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from mi_tienda.models import London
+from mi_tienda.models import Pisa
+from mi_tienda.models import Paris
+
+# Register your models here.
+admin.site.register(London)
+admin.site.register(Pisa)
+admin.site.register(Paris)
