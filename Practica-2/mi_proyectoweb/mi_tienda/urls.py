b0cdf6661072a243b6dda215c49bd2317a53f9ef
Leyree <leyremc@MacBook-Pro-de-Leyree.local>
2019-03-09 10:48:36 +0100

    Añadidos index 'secundarios'

--- Practica-2/mi_proyectoweb/mi_tienda/urls.py
+++ Practica-2/mi_proyectoweb/mi_tienda/urls.py
@@ -7,7 +7,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home_view),
     url(r'^admin/', admin.site.urls),
-    #url(r'^$', views.london_view),
-    #url(r'^$', views.pisa_view),
-    #url(r'^$', views.paris_view),
+    url(r'^london', views.london_view),
+    url(r'^pisa', views.pisa_view),
+    url(r'^paris', views.paris_view),
 ]
