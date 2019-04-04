5f5f7870401b3000024f6c397ff7eeb106b0c025
Leyree <leyremc@MacBook-Pro-de-Leyree.local>
2019-04-04 17:24:08 +0200

    Añadiendo archivos --

--- Practica-2/mi_proyectoweb/mi_tienda/urls.py
+++ Practica-2/mi_proyectoweb/mi_tienda/urls.py
@@ -7,6 +7,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home_view),
     url(r'^admin/', admin.site.urls),
+    url(r'^index', views.home_view),
     url(r'^london', views.london_view),
     url(r'^pisa', views.pisa_view),
     url(r'^paris', views.paris_view),
